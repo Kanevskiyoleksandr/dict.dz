3c433d480ed58397423fc3737cc2d62b5d172105
Kanevskiyoleksandr <60288893+Kanevskiyoleksandr@users.noreply.github.com>
2020-02-06 16:27:51 +0200

    first commit

---
+++ dz5.1.py
@@ -0,0 +1,5 @@
+a = dict({'Dark Knight' : 'Batman', 'Man of Steel' :'Superman'})
+b = dict({'Enemy' : 'Joker', 'Oponent':'Lex luthor'})
+c = dict({'City':'Gotham', 'Location' : 'Metropolis'})
+f = {**a,**b,**c}
+print(f)
